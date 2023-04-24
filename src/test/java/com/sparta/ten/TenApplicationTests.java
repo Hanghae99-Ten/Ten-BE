7a1bc54b8a8cc0f8149c8657ffd5ef3e2c78a899
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-24 23:57:29 +0900

    Setting: 기본 세팅

---
+++ src/test/java/com/sparta/ten/TenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
