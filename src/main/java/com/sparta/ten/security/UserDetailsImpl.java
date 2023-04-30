4f6636f3b190ed2be90e60789e2b57369567b2e4
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-30 22:46:48 +0900

    feat: UserDetailsImpl 생성

---
+++ src/main/java/com/sparta/ten/security/UserDetailsImpl.java
@@ -0,0 +1,69 @@
+package com.sparta.ten.security;
+
+import com.sparta.sogonsogon.member.entity.Member;
+import com.sparta.sogonsogon.member.entity.MemberRoleEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Slf4j
+public class UserDetailsImpl implements UserDetails {
+
+    private final Member user;
+    private final String username;
+
+    public UserDetailsImpl(Member user, String username) {
+        this.user = user;
+        this.username = username;
+    }
+
+    public Member getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        MemberRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
