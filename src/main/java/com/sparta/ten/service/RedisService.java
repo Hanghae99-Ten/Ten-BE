74b0333ffc15397a4ac536c0e92137f57c483b31
Jjuhui313 <hyacinth92928@gmail.com>
2023-10-06 17:58:24 +0900

    PSPU-5 chore: QueryDSL 확장자 추가 후 Redis 관련 확장자 및 코드 주석처리, test: QueryDSL 추가 후 testCode 작성

--- src/main/java/com/sparta/ten/service/RedisService.java
+++ src/main/java/com/sparta/ten/service/RedisService.java
@@ -1,31 +1,31 @@
-package com.sparta.ten.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.time.Duration;
-
-@Service
-@RequiredArgsConstructor
-public class RedisService {
-    private final RedisTemplate redisTemplate;
-
-    // 데이터 넣기
-    public void setValues(String key, String value){
-        ValueOperations<String, String> values = redisTemplate.opsForValue();
-        values.set(key, value, Duration.ofMinutes(3)); // 3분 뒤 메모리에서 삭제된다.
-    }
-    // 데이터 가져오기
-    public String getValues(String key){
-        ValueOperations<String, String> values = redisTemplate.opsForValue();
-        String value = values.get(key);
-        if(value == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-        return value;
-    }
-}
+//package com.sparta.ten.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.ValueOperations;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import java.time.Duration;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class RedisService {
+//    private final RedisTemplate redisTemplate;
+//
+//    // 데이터 넣기
+//    public void setValues(String key, String value){
+//        ValueOperations<String, String> values = redisTemplate.opsForValue();
+//        values.set(key, value, Duration.ofMinutes(3)); // 3분 뒤 메모리에서 삭제된다.
+//    }
+//    // 데이터 가져오기
+//    public String getValues(String key){
+//        ValueOperations<String, String> values = redisTemplate.opsForValue();
+//        String value = values.get(key);
+//        if(value == null) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+//        }
+//        return value;
+//    }
+//}
