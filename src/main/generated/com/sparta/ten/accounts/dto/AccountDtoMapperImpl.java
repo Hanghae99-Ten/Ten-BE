b86664f806f4a8243af01789b373d20c1b64af76
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2024-04-24 21:45:12 +0900

    [PSPU-12]Fix: @OneToMany -> @ManyToOne 변경

--- src/main/generated/com/sparta/ten/accounts/dto/AccountDtoMapperImpl.java
+++ src/main/generated/com/sparta/ten/accounts/dto/AccountDtoMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-04-24T21:41:56+0900",
+    date = "2024-04-24T21:42:51+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 17.0.10 (Amazon.com Inc.)"
 )
 @Component
